8d71ab483f651a9111f707137787c89b5e4400f0
Vimal Tripathi <138547942+vimaltripathi18@users.noreply.github.com>
2024-10-27 22:00:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,11 @@ connectDB();
 connectCloudinary();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://admin.piiwear.com',
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true
+}));
 
 app.use('/api/user', userRouter);
 app.use('/api/product',productRouter);
